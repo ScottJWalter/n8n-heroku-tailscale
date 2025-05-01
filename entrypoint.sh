9e30a2030450215b3c2d91f37672b48ed40fa8fc
Scott Walter <sjwalter@gmail.com>
2025-05-01 20:40:47 +0000

    fix tailscale path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_NODE_NAME}
+/tailscale/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+/tailscale/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_NODE_NAME}
 echo Tailscale started
 
 # check if port variable is set or go with default
