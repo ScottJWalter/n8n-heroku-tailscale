43d9f3908da77e03ed51f23b1a96c7f27bdd8bca
Scott Walter <sjwalter@gmail.com>
2025-05-01 21:28:01 +0000

    tailscale tweak

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-/tailscale/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
-/tailscale/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_NODE_NAME}
+/tailscale/tailscaled \
+  --tun=userspace-networking \
+  --socks5-server=localhost:1055 \
+  --outbound-http-proxy-listen=localhost:1055 \
+  &
+
+/tailscale/tailscale \
+  --auth-key=${TAILSCALE_AUTHKEY} \
+  --hostname=${TAILSCALE_NODE_NAME} \
+  --accept-routes=true \
+  up 
+
 echo Tailscale started
 
 # check if port variable is set or go with default
